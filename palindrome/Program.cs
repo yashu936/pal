afe2a2799c8ef60ed37abb9265a0ea34d1c36cba
yashu936 <kyaswanthnaidu436@gmail.com>
2024-08-21 10:23:57 -0400

    Add project files.

---
+++ palindrome/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+
+            if (IsPalindrome(input))
+            {
+                Console.WriteLine($"'{input}' is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine($"'{input}' is not a palindrome.");
+            }
+            Console.ReadLine();
+        }
+
+        static bool IsPalindrome(string s)
+        {
+            int left = 0;
+            int right = s.Length - 1;
+
+            while (left < right)
+            {
+                if (char.ToLower(s[left]) != char.ToLower(s[right]))
+                {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+    }
+}
